ccff685c09b2a354b7986f22d1f0ff093ab6b445
KlausmullerDev <klaus.muller@sempreceub.com>
2025-09-09 19:12:36 -0300

    Apaga todos os arquivos da branch frontend

--- src/types/express/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { UserPublic } from '../../services/user.service';
-
-declare global {
-    namespace Express {
-        export interface Request {
-            user?: UserPublic;
-        }
-    }
-}
