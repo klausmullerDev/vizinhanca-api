e09b4f9d0dea5814c4a0d5fcb2f058106ed010b3
KlausmullerDev <klaus.muller@sempreceub.com>
2025-09-09 10:52:45 -0300

    fix: ajustando o CORS

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './swagger';
 import userRouter from './routes/user.routes';
@@ -8,6 +9,9 @@ const app = express();
 
 
 app.use(express.json());
+
+app.use(cors());
+
 
 
 app.use('/users', userRouter); 
